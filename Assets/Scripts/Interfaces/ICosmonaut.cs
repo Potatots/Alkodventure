81b2fbb3dc9f98bd8aaa1540f3e13d427197649b
potatots <inny007@interia.pl>
2017-12-08 21:54:42 +0100

    added GameManager

--- Assets/Scripts/Interfaces/ICosmonaut.cs
+++ Assets/Scripts/Interfaces/ICosmonaut.cs
@@ -9,10 +9,17 @@ public interface ICosmonaut {
     int Adrenalin { get; set; }
     int Endorfines { get; set; }
     int Alcohol { get; set; }
+    int Accuracy { get; set; }
 
     int AmmoLeft { get; set; }
     int HealthLeft { get; set; }
 
-    int Accuracy { get; set; }
-    int Speed { get; set; }
+    float Speed { get; set; }
+
+    float maxSpeed { get; set; }
+    int maxValue { get; set; } //max Value of adrenalin/endorfines/alcohol
+    int maxHealth { get; set; }
+    int maxAmmo { get; set; }
+
+    Rigidbody2D CosmoRigidbody { get; set; }
 }
