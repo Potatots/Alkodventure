200e8c23e68aa7bdf18f8f20c8743254656fea27
ccerlew007 <yfgaghguaygwe@gmail.com>
2017-12-09 16:07:24 +0100

    UIManager

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Assets.Scripts.Cosmonaut;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
@@ -9,7 +10,16 @@ public class UIManager : MonoBehaviour {
 	private Cosmonaut cosmoscript;
 	private int adrenalin;
 	private int health;
+	private int alcohol;
 	private int happiness;
+	public Slider adrenalinImage;
+	public Slider healthImage;
+	public Slider happinessImage;
+	public Slider alcoImage;
+
+
+	[SerializeField]
+	private float  fill = 0.2f;
 
 
 	void Start () {
@@ -30,11 +40,19 @@ public class UIManager : MonoBehaviour {
 		adrenalin = cosmonaut.GetComponent<Cosmonaut> ().Adrenalin;
 		health = cosmonaut.GetComponent<Cosmonaut> ().Alcohol;
 		happiness = cosmonaut.GetComponent<Cosmonaut> ().HealthLeft;
+		alcohol = cosmonaut.GetComponent<Cosmonaut> ().Alcohol;
 		updateValue (ref adrenalin);
 		updateValue (ref health);
 		updateValue (ref happiness);
+		updateValue (ref alcohol);
 
 		Debug.Log ("adrenalin: " + adrenalin + " health: " + health + "happiness: " + happiness); 
+
+		adrenalinImage.value = (float) (adrenalin / 100.0f);
+		healthImage.value =(float)  (health / 100.0f);
+		happinessImage.value =(float)  (happiness / 100.0f);
+		alcoImage.value =(float) ( alcohol/ 100.0f);
+
 
 	}
 }
